0d3e6b24063d39af8668ffd5c2f0a03b4ef9f152
Erős István <eros@zold.lan>
2025-01-15 19:55:05 +0100

    rest

--- src/app/base.service.ts
+++ src/app/base.service.ts
@@ -1,26 +1,42 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BaseService {
   apiUrl="http://localhost:3000/techcucc/"
-  constructor(private http:HttpClient) { }
+  private adatSub=new Subject()
+  constructor(private http:HttpClient) {
+    this.downloadAll()
+   }
 
   getAll(){
-    return this.http.get(this.apiUrl)
+    return this.adatSub
+  }
+
+  private downloadAll(){
+    this.http.get(this.apiUrl).subscribe(
+      (res)=>this.adatSub.next(res)
+    )
   }
 
   newData(data:any){
-    this.http.post(this.apiUrl,data).subscribe()
+    this.http.post(this.apiUrl,data).forEach(
+      ()=>this.downloadAll()
+    )
   }
   updateData(data:any){
-    this.http.put(this.apiUrl+data.id,data).subscribe()
+    this.http.put(this.apiUrl+data.id,data).forEach(
+      ()=>this.downloadAll()
+    )
   }
-  
+
   deleteData(data:any){
-    this.http.delete(this.apiUrl+data.id).subscribe()
+    this.http.delete(this.apiUrl+data.id).forEach(
+      ()=>this.downloadAll()
+    )
   }
 
 
